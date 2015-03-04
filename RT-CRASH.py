e1001bce827763fa5c5a14a9d0533f0da6ab2833
Diego <expl01t3r@users.noreply.github.com>
2015-03-04 17:10:03 -0300

    Update RT-CRASH.py

--- RT-CRASH.py
+++ RT-CRASH.py
@@ -1,3 +1,12 @@
+import hashlib
+import argparse
+import sys
+import os
+
+
+
+
+os.system('cls' if os.name == 'nt' else 'clear')
 print('''
 ██████╗ ████████╗     ██████╗██████╗  █████╗ ███████╗██╗  ██╗
 ██╔══██╗╚══██╔══╝    ██╔════╝██╔══██╗██╔══██╗██╔════╝██║  ██║
@@ -6,19 +15,13 @@ print('''
 ██║  ██║   ██║       ╚██████╗██║  ██║██║  ██║███████║██║  ██║
 ╚═╝  ╚═╝   ╚═╝        ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
                          MD5 SHA1 SHA224 SHA256 SHA384 SHA512                                    
-
-
 [+] Diego Bernardes
 [+]https://breaksec.wordpress.com/
-''')
-import hashlib
-import argparse
-import sys
-
+''')  
 def progress():
 	sys.stdout.write("\r")
-	sys.stdout.write('Tentando senha: {senha} Para a hash {tmphash}'
-	.format(senha = str(x).rstrip(), tmphash = busca))
+	sys.stdout.write('{senha}'
+	.format(senha = str(x).rstrip()))
 	
 argumentos = argparse.ArgumentParser()
 argumentos.add_argument('--tipo', action = 'store', dest = 'tipo', required = True, help = '''
@@ -32,7 +35,6 @@ argumentos.add_argument('--tipo', action = 'store', dest = 'tipo', required = Tr
 argumentos.add_argument('--hash', action = 'store', dest = 'hash', required = True, help = 'Hash que você deseja quebrar')
 argumentos.add_argument('--wlist', action = 'store', dest = 'wordlist', required = True, help = 'Caminho da Wordlist (Ex.: C:\wordlist.txt )')
 arg = argumentos.parse_args()
-
 
 text = open(arg.wordlist)
 busca = str(arg.hash).lower()
@@ -57,20 +59,24 @@ else:
 	print('Tipo de hash inválido')
 	exit()
 
+print('''
+Hash: {hashx}
+Tentando senha:''' .format(hashx = busca))
+try:
+    for x in text:
+        temp = hashFunc(x.rstrip().encode('utf-8')).hexdigest()
+        progress()
+        if busca == temp:
+            print('\n\n[+]Valor encontrado\nHash: {hash}\nTexto Original: {text}Numero de tentativas executadas: {cnt}'
+                  .format(hash = busca, text = x, cnt = contador))
+            opcode = 1
+            break
+        contador += 1
+except KeyboardInterrupt:
+    print('\n\n[-]Procedimento cancelado pelo usuário\nNumero de tentativas executadas: {cnt}' .format(cnt = contador))
+    exit(1)
 
-for x in text:
-    temp = hashFunc(x.rstrip().encode('utf-8')).hexdigest()
-    progress()
-    if busca == temp:
-        print('\n\n[+]Valor encontrado\nHash: {hash}\nTexto Original: {text}{xD}\nNumero de tentativas executadas: {cnt}' .format(
-            hash = busca, text = x, xD = ('-'*30), cnt = contador))
-        opcode = 1
-        break
-    contador += 1 
-	
+    
 text.close()
 if opcode == 0:
-	print('Valor não encontrado, tente com uma wordlist mais completa ;)\nNumero de tentativas executadas: {cnt}' .format(cnt = contador))
-else:
-	print('Finalizado')
-
+	print('\n\n[-]Valor não encontrado, tente com uma wordlist mais completa ;)\nNumero de tentativas executadas: {cnt}' .format(cnt = contador))
